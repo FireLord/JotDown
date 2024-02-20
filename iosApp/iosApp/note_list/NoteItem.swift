f03fe360d8c4303eb7721ffef3bc4c8956d41b53
FireLord <firelord.xda@gmail.com>
2024-02-20 23:58:58 +0530

    iosApp: Create NoteItem view

---
+++ iosApp/iosApp/note_list/NoteItem.swift
@@ -0,0 +1,55 @@
+//
+//  NoteItem.swift
+//  JotDown
+//
+//  Created by Aman Kumar on 20/02/24.
+//  Copyright © 2024 orgName. All rights reserved.
+//
+
+import SwiftUI
+import shared
+
+struct NoteItem: View {
+    var note: Note
+    var onDeleteClick: () -> Void
+    
+    var body: some View {
+        VStack(alignment: .leading) {
+            HStack{
+                Text(note.title)
+                    .font(.title3)
+                    .fontWeight(.semibold)
+                Spacer()
+                Button(action: onDeleteClick) {
+                    Image(systemName: "xmark")
+                        .foregroundColor(.black)
+                }
+            }.padding(.bottom, 3)
+            
+            Text(note.content)
+                .fontWeight(.light)
+                .padding(.bottom, 3)
+            HStack {
+                Spacer()
+                Text(DateTimeUtil().formatNoteDate(dateTime: note.created))
+                    .font(.footnote)
+                    .fontWeight(.light)
+            }
+        }
+        .padding()
+        .background(Color(hex: note.colorHex))
+        .clipShape(RoundedRectangle(cornerRadius: 5.0))
+    }
+}
+
+#Preview {
+    NoteItem(
+        note: Note(
+            id: nil,
+            title: "My note",
+            content: "Note content",
+            colorHex: 0xFF2341,
+            created: DateTimeUtil().now()),
+        onDeleteClick: {}
+    )
+}
