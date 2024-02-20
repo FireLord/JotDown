005ec260c6e4d4ab54727d1baa9cfb7d0bce8102
FireLord <firelord.xda@gmail.com>
2024-02-20 23:28:14 +0530

    iosApp: Create noteListViewModel

---
+++ iosApp/iosApp/note_list/NoteListScreen.swift
@@ -0,0 +1,19 @@
+//
+//  NoteListScreen.swift
+//  JotDown
+//
+//  Created by Aman Kumar on 20/02/24.
+//  Copyright © 2024 orgName. All rights reserved.
+//
+
+import SwiftUI
+
+struct NoteListScreen: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    NoteListScreen()
+}
