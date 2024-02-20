98bcf894230861bc9f423789f4c7afde4119e42c
FireLord <firelord.xda@gmail.com>
2024-02-20 20:45:22 +0530

    androidApp: Fix Search icon for textField

--- androidApp/src/main/java/com/firelord/jotdown/android/note_list/HideableSearchTextField.kt
+++ androidApp/src/main/java/com/firelord/jotdown/android/note_list/HideableSearchTextField.kt
@@ -9,6 +9,7 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Close
+import androidx.compose.material.icons.filled.Search
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.OutlinedTextField
@@ -65,7 +66,7 @@ fun HideableSearchTextField(
         ) {
             IconButton(onClick = onCloseClick) {
                 Icon(
-                    imageVector = Icons.Filled.Close,
+                    imageVector = Icons.Filled.Search,
                     contentDescription = "Open search"
                 )
             }
